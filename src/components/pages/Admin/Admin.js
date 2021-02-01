fa08eebf5ff79596c5f94785199af0cf719720c4
Andrew Sedore <asedore1@myseneca.ca>
2021-02-01 15:09:53 -0500

    changing to hooks

--- src/components/pages/Admin/Admin.js
+++ src/components/pages/Admin/Admin.js
@@ -1,26 +1,41 @@
-import React, { Component } from "react"
+import React, { useState, Component } from "react"
 import Form from "../Form/Form"
 
-class Admin extends Component {
-	state = {
-		location: " ",
-		start: 0,
-		end: 0,
-	}
+// class Admin extends Component {
+// 	state = {
+// 		location: " ",
+// 		start: 0,
+// 		end: 0,
+// 	}
 
-	submitHandler(loc, start, end, day) {
-		this.props.submit(loc, start, end, day)
-	}
+// 	submitHandler(loc, start, end, day) {
+// 		this.props.submit(loc, start, end, day)
+// 	}
 
-	render() {
-		console.log(this.props)
+// 	render() {
+// 		console.log(this.props)
 
-		return (
-			<div>
-				<Form submit={(loc, start, end) => this.submitHandler(loc, start, end, "Monday")} />
-			</div>
-		)
-	}
+// 		return (
+// 			<div>
+// 				<Form submit={(loc, start, end) => this.submitHandler(loc, start, end, "Monday")} />
+// 			</div>
+// 		)
+// 	}
+// }
+
+// export default Admin
+
+// import React from 'react';
+
+const Admin = () => {
+	const [monStart, setMonStart] = useState(" ")
+
+	return (
+		<div>
+			<input name="monStart" value={monStart} onChange={(e) => setMonStart(e.target.value)}></input>
+			<span>{monStart}</span>
+		</div>
+	)
 }
 
 export default Admin
--- src/components/pages/Shedule/Schedule.js
+++ src/components/pages/Shedule/Schedule.js
@@ -1,8 +1,9 @@
 import React from "react"
+import Admin, { setState } from "../Admin/Admin"
 
 const schedule = (props) => {
 	console.log(props)
-	return <div></div>
+	return <div>monday start time : {Admin.monStart}</div>
 }
 
 export default schedule
